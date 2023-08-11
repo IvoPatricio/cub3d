9a825dafa909e8c2027b5489a544f6ac6e9bc401
IvoPatricio <ifreire-@c2r8s2.42lisboa.com>
2023-08-11 19:18:09 +0100

    nothing special

--- srcs/main.c
+++ srcs/main.c
@@ -77,7 +77,7 @@ void ft_key_a(t_map *map, int key_code)
 			(int)PLAYER_SPEED)][(int)map->play->posX] == FLOOR)
 			map->play->posY -= map->play->plane_y * PLAYER_SPEED;
 			if (map->map[((int)map->play->posY - (int)map->play->plane_y * \
-			(int)PLAYER_SPEED)][(int)map->play->posX] == WALL)
+				(int)PLAYER_SPEED)][(int)map->play->posX] == WALL)
 				map->play->posY = tempY;
 	}
 }
@@ -95,22 +95,24 @@ void ft_key_d(t_map *map, int key_code)
 			(int)map->play->plane_x * (int)PLAYER_SPEED)]) == FLOOR)
 			map->play->posX += map->play->plane_x * PLAYER_SPEED;
 			if ((map->map[(int)map->play->posY][((int)map->play->posX + \
-			(int)map->play->plane_x * (int)PLAYER_SPEED)]) == WALL)
+				(int)map->play->plane_x * (int)PLAYER_SPEED)]) == WALL)
 				map->play->posX = tempX;
 		if (map->map[((int)map->play->posY + (int)map->play->plane_y * \
 			(int)PLAYER_SPEED)][(int)map->play->posX] == FLOOR)
 			map->play->posY += map->play->plane_y * PLAYER_SPEED;
 			if (map->map[((int)map->play->posY + (int)map->play->plane_y * \
-			(int)PLAYER_SPEED)][(int)map->play->posX] == WALL)
+				(int)PLAYER_SPEED)][(int)map->play->posX] == WALL)
 				map->play->posY = tempY;
 	}
 }
 
-void ft_key_angle(t_play *play, double rotate)
+void ft_key_angle(t_map *map, double rotate)
 {
+	t_play	*play;
 	double	old_dir_x;
 	double	old_plane_x;
 
+	play = map->play;
 	printf_struct_player(play);
 	old_dir_x = play->dir_x;
 	play->dir_x = play->dir_x * cos(rotate) - play->dir_y * sin(rotate);
@@ -143,20 +145,21 @@ void	draw_texture(t_map *map, int x, int color)
 
 void	draw_walls(t_map *map, int x)
 {
-	if (map->ray->side == 1)
+	draw_texture(map, x, 0x0000FF);
+	/*if (map->ray->side == 1)
 	{
-		if (map->ray->stepy < 0)
+		if (map->ray->stepy  == -1)
 			draw_texture(map, x, 0xFF0000);
 		else 
 			draw_texture(map, x, 0x00FF00);
 	}
 	if (map->ray->side == 0)
 	{
-		if (map->ray->stepx < 0)
+		if (map->ray->stepx == -1)
 			draw_texture(map, x, 0x0000FF);
 		else 
 			draw_texture(map, x, 0x000000);
-	}
+	}*/
 }
 
 void draw_background(t_map *map, int x)
@@ -164,11 +167,11 @@ void draw_background(t_map *map, int x)
 	int	y;
 
 	y = 0;
-	while (y < WIN_Y)
+	while (y < (int)WIN_Y)
 	{
 		if (y < map->ray->drawstart)
 			ft_pixel_drawing(map->mlx, x, y, map->texture->roof);
-		else if (y >= map->ray->drawend)
+		else if (y > map->ray->drawend)
 			ft_pixel_drawing(map->mlx, x, y, map->texture->floor);
 		y++;
 	}
@@ -205,13 +208,13 @@ void raycast_dda(t_map *map)
 		
 		ray->line_height = (int)(WIN_Y / ray->walldist);
 
-		ray->drawstart = -ray->line_height / 2 + WIN_Y / 2;
+		ray->drawstart = -ray->line_height / 2 + (int)WIN_Y / 2;
 		if (ray->drawstart < 0)
 			ray->drawstart = 0;
 
-		ray->drawend = ray->line_height / 2 + WIN_Y / 2;
-		if (ray->drawend >=  WIN_Y)
-			ray->drawend = WIN_Y - 1;
+		ray->drawend = ray->line_height / 2 + (int)WIN_Y / 2;
+		if (ray->drawend >=  (int)WIN_Y)
+			ray->drawend = (int)WIN_Y - 1;
 	}
 	if (ray->side == 0)
 		ray->wallhit = map->play->posY + ray->walldist * ray->rayDirY;
@@ -257,11 +260,13 @@ void raycast_1(t_map *map, int i)
 	t_ray	*ray;
 
 	ray = map->ray;
-	ray->cameraX = 2 * i /(double)WIN_X-1;
+	ray->cameraX = 2 * i /(double)WIN_X - 1;
 	ray->rayDirX = map->play->dir_x + map->play->plane_x * ray->cameraX;
 	ray->rayDirY = map->play->dir_y + map->play->plane_y * ray->cameraX;
 	ray->mapX = (int)map->play->posX;
 	ray->mapY = (int)map->play->posY;
+	//printf("MAP:X%d\n", ray->mapX);
+	//printf("MAP:Y%d\n", ray->mapY);
 	ray->deltaDistX = sqrt(1 + (ray->rayDirY* ray->rayDirY) \
 		/ (ray->rayDirX * ray->rayDirX));
 	ray->deltaDistY = sqrt(1 + (ray->rayDirX * ray->rayDirX) \
@@ -274,7 +279,7 @@ void raydrawing(t_map *map)
 	int x;
 
 	x = 0;
-	while (x < WIN_X)
+	while (x < (int)WIN_X)
 	{
 		raycast_1(map, x);
 		draw_background(map, x);
@@ -298,9 +303,9 @@ int	ft_keys(int key_code, t_map *map)
 	if (key_code == D_KEY)
 		ft_key_d(map, key_code);
 	if (key_code == RIGHT)
-		ft_key_angle(map->play, -(double)ROTATE_SPEED);
+		ft_key_angle(map, -(double)ROTATE_SPEED);
 	if (key_code == LEFT)
-		ft_key_angle(map->play, (double)ROTATE_SPEED);
+		ft_key_angle(map, (double)ROTATE_SPEED);
 	raydrawing(map);
 	return (0);
 }
@@ -310,8 +315,8 @@ void ft_mlx_init(t_map *map)
 	map->mlx->mlx_ptr = mlx_init();
 	if (map->mlx->mlx_ptr != 0)
 	{
-		map->mlx->win_ptr = mlx_new_window(map->mlx->mlx_ptr, WIN_X, WIN_Y, "3D");
-		map->mlx->img_to_window = mlx_new_image(map->mlx->mlx_ptr, WIN_X, WIN_Y);
+		map->mlx->win_ptr = mlx_new_window(map->mlx->mlx_ptr, (int)WIN_X, (int)WIN_Y, "3D");
+		map->mlx->img_to_window = mlx_new_image(map->mlx->mlx_ptr, (int)WIN_X, (int)WIN_Y);
 		map->mlx->buffer = mlx_get_data_addr(map->mlx->img_to_window, &map->mlx->a,
 			&map->mlx->b, &map->mlx->c);
 		if (!map->mlx->buffer)
